bd566059a2ad703d334d669b8ff301a9c623eaa3
mwambajoakim <mwambajoakim@gmail.com>
2022-12-20 07:51:35 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-return(0);
+return (0);
 }
