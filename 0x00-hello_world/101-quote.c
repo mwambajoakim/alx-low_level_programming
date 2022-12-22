e12e6ed533635b3a1bf1fd2b1abe427076e7dee6
mwambajoakim <mwambajoakim@gmail.com>
2022-12-22 06:57:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  *main - Entry point
  *return always 1: Success
  **/
 int main(void)
 {
-  write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
