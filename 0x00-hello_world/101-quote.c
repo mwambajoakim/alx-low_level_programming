9bcd891590543cc476fe89c34c78190987603321
mwambajoakim <mwambajoakim@gmail.com>
2022-12-22 06:42:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *return always 1: Success
+ **/
+int main(void)
+{
+  write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
