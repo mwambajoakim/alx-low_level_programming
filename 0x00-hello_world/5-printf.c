868eeeecf1450404c98039acac80dc8c8b5a30e3
mwambajoakim <mwambajoakim@gmail.com>
2023-03-16 05:33:15 -0700

    The printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 int main(void)
 {
-  /**
-   *main - Entry point
-   *
-   *Return value 0: Success
-   **/
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*
+**/
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
