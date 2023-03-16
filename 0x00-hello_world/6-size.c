bd0bfbd0f9b006fae98b5015c69de688b5be1f73
mwambajoakim <mwambajoakim@gmail.com>
2023-03-16 05:58:54 -0700

    Size of different data structures

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,25 +2,20 @@
 
 int main(void)
 
+
+/**
+  *main - entry point
+  *
+  *Return value 0: Success
+  *
+  */
+  int main(void)
 {
-  /*
-   *main - entry point
-   *
-   *Return value 0: Success
-   *
-   */
-
-  int a;
-  char b;
-  long int c;
-  long long int d;
-  float e;
-
-printf("Size of an int is: %li byte(s)\n", sizeof(a));
-printf("Size of a char is: %li byte(s)\n", sizeof(b));
-printf("Size of a long int is: %li byte(s)\n" sizeof(c));
-printf("Size of a long long int is: %li byte(s)\n", sizeof(d));
-printf("Size of a float is: %li byte(s)\n", sizeof(e));
+printf("Size of an int is: %li byte(s)\n", sizeof(int));
+printf("Size of a char is: %li byte(s)\n", sizeof(char));
+printf("Size of a long int is: %li byte(s)\n" sizeof(long int));
+printf("Size of a long long int is: %li byte(s)\n", sizeof(long long int));
+printf("Size of a float is: %li byte(s)\n", sizeof(float));
 return (0);
 
 }
