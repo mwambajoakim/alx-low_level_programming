ed8338d34655e750f84eac7a0130c17816aa3011
mwambajoakim <mwambajoakim@gmail.com>
2023-03-16 05:46:10 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 int main(void)
 {
 /**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*
-*/
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
