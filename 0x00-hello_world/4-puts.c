2ed6fb90f25b219dd00b4b572f4b3874cfff02cb
mwambajoakim <mwambajoakim@gmail.com>
2023-03-16 03:49:22 -0700

    The put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-{
   /*
    *main - Entry point
    *
    *Return value 0: Success
    */
-
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
