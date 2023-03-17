75fe858f7ceeb13282b7c374b5e33af785287e3f
mwambajoakim <mwambajoakim@gmail.com>
2023-03-17 14:03:34 -0700

    Printing lower and uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Use `putchar` to print lowercase and then uppercase alphabet.
+ * Return: 0
+ */
+
+int main(void)
+{
+char a = 'a';
+
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+
+a = 'A'
+while (a <= 'Z')
+{
+putchar(a);
+a++;
+}
+return (0);
+}
+
+  
