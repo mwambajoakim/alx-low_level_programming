715fefb71dbae5ecec1fab561d2bd313d8ab66dc
mwambajoakim <mwambajoakim@gmail.com>
2023-03-17 04:51:44 -0700

    Incrementing numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+  char d=1;
+
+  for(d='1'; d<='9'; d++)
+    putchar(d);
+  return (0);
+}
