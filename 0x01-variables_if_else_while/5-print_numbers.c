f8d1c32fcec4de3a28247c7eb13c723c5a599ffb
mwambajoakim <mwambajoakim@gmail.com>
2023-03-20 00:11:53 -0700

    Single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**main -Entry point
+ *
+ *Return: 0 always Success
+ *
+ */
+
+int main(void)
+{
+int a = '0';
+
+while (a <= 10)
+a++
+{
+printf("%d\n", a);
+}
+return (0);
+}
