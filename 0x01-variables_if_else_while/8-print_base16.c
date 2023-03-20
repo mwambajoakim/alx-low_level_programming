8ee679858531d9ed79ad7f5d2a8f4d6225832cfd
mwambajoakim <mwambajoakim@gmail.com>
2023-03-20 03:04:24 -0700

    Hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return; 0 is always success
+ *
+ */
+
+int main(void)
+{
+char c;
+int k;
+
+for (k = 0; k < 10; k++)
+{
+putchar(k + '0');
+}
+for (c = 'a'; c < 'g'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+
+return (0);
+}
