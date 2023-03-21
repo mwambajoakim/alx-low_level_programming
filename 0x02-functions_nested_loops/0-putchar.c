ab94f75869192a5a1dfda9eee544fc9f86d10eb1
mwambajoakim <mwambajoakim@gmail.com>
2023-03-21 03:24:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 is success
+ *
+ */
+
+int main(void)
+{
+char a = 'putchar';
+
+putchar('_');
+putchar(a);
+
+return (0);
+}
