d758ffe3f090ae7aaa6ae2094261e2532ecdef4d
mwambajoakim <mwambajoakim@gmail.com>
2023-03-22 01:16:20 -0700

    Update on main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+in t_isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
