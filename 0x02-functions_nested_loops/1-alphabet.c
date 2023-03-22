02b7ad0d596581fe089775142efb40ddf6392e1f
mwambajoakim <mwambajoakim@gmail.com>
2023-03-22 01:35:30 -0700

    First alphabet protottype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0
+ *
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+char a = 'a';
+
+while (a <= 'z')
+{
+_putchar(a);
+_putchar('\n');
+}
+return (0);
+  
+print_alphabet(char a)
+{
+char results;
+results = a;
+return results;
+}
