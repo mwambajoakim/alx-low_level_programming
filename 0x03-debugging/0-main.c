3e566b38080a8370d9479a8b0c4c56e3ddaec2d2
mwambajoakim <mwambajoakim@gmail.com>
2023-03-22 05:39:45 -0700

    Positive or negative number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *positive_or_negative - function checks if number is positive or negative
+ *
+ *Return: 0 is success
+ *
+ */
+
+void positive_or_negative(int i)
+{
+i = 0;
+
+positive_or_negative(i);
+}
