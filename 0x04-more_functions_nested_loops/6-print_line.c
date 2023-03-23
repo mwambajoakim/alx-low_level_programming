73d8fe9caa1ac3fff5a9a1cd9dd415571737ed86
mwambajoakim <mwambajoakim@gmail.com>
2023-03-23 01:28:25 -0700

    Printing a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_line - function that will print a straight line
+ *
+ *Return: nothing
+ *
+ */
+
+void print_line(int n)
+{
+line = '_';
+n = line * 10;
+
+ _putchar(n);
+}
