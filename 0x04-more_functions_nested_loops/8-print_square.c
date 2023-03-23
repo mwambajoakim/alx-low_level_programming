ff377cfc4479e72d0d1b72a7251fcd53c46d9767
mwambajoakim <mwambajoakim@gmail.com>
2023-03-23 02:00:53 -0700

    Printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_square - function tha prints a square
+ *
+ *Return: nothing
+ *
+ */
+
+void print_square(int size)
+{
+if (size > 0)
+
+int nl;
+for ( nl = 0; nl < size; nl++)
+{
+for (wd = 0; wd < nl; wd++)
+{
+_putchar('#');
+
+if (nl == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n')
+}
