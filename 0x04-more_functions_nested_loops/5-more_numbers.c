fb140cc4cc5f7fee1b6f142da5485e5a0a0eb5b2
mwambajoakim <mwambajoakim@gmail.com>
2023-03-23 01:21:53 -0700

    Printing numbers 0 through 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *more_numbers - function that will print numbers 0-14 ten times
+ *
+ *Return: nothing
+ *
+ */
+
+void more_numbers(void)
+{
+int c = 0;
+
+while (c < 11)
+{
+int v = 0;
+while ( v <= 14)
+{
+ _putchar(v);
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
