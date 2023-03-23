aae56cf183c90fd06dee5f693c683f94e7093336
mwambajoakim <mwambajoakim@gmail.com>
2023-03-23 01:01:50 -0700

    Printing numbers 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#includde "main.h"
+
+/**
+ * print_numbers - function that prints numbers 0-9
+ *
+ *Return: nothing
+ *
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
