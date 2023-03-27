59dee55074c883a1c7c2c88f014d43a89b3cb317
mwambajoakim <mwambajoakim@gmail.com>
2023-03-27 14:04:08 +0300

    A function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - Function that prints a string
+ *
+ *@str: String to be tested
+ *
+ *Return: nothing
+ *
+ */
+
+void _puts(char *str)
+{
+puts(*str);
+puts('\n');
+return;
+}
