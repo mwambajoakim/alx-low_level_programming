00dbd414538fee1cdde606663fbfadf3c46a8d7a
mwambajoakim <mwambajoakim@gmail.com>
2023-03-27 14:34:42 +0300

    A funtion that will print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *rev_string - funtion that reverses a string
+ *
+ *@s: the string to be tested
+ *
+ *Return: nothing
+ *
+ */
+
+void rev_string(char *s)
+{
+int d;
+for (d = strlen(s) - 1; d >= 0; d--)
+{
+}
+return;
+}
