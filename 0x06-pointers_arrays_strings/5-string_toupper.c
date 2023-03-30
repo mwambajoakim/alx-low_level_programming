5588751e5ac17e9376903cedf3ac4ddab4d173c9
mwambajoakim <mwambajoakim@gmail.com>
2023-03-30 21:13:40 +0300

    A fuunction that will change characters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,20 @@
-include "main.h"
-#include <stdio.h>
-
+#include "main.h"
 /**
- * string_toupper - function that changes all lowercase of a sting to uppercase
- * @s: string to be reversed
- * Return: return value s
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
  */
-
 char *string_toupper(char *s)
 {
+
 int i;
 
-for (i = 0; s[i] != '\0'; i++)
+i = 0;
+while (*(s + i))
 {
-if (s[i] >= 'a' && s[i] <= 'z')
-{
-s[i] -= 32;
-}
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
 }
 return (s);
 }
