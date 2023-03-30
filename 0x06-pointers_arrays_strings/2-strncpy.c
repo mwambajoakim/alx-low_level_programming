a20ae2a3960b65800eff1c8066b48f840d8577f7
mwambajoakim <mwambajoakim@gmail.com>
2023-03-30 11:12:33 +0300

    A function that copies a string to another

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strncpy - function that copies a string to another
+ *
+ *@dest: string to be tested
+ *@src: string to be tested
+ *@n: amount of characters to copy
+ *
+ *Return: value of dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int k;
+for (k = 0; k < n && src[k] != '\0'; k++)
+dest = dest[k] + src[k];
+for (; k < n; k++)
+dest[k] = '\0';
+return (dest);
+}
