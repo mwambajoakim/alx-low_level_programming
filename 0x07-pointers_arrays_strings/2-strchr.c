9ac6e70d2a9ecc98fc4f49fa8fed0e56efee2511
mwambajoakim <mwambajoakim@gmail.com>
2023-04-03 22:26:11 +0300

    A function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strchr - function that locates a character in a string
+ *
+ *@s: string to be checked
+ *@c: character to be checked
+ *
+ *Return: pointer to occerence of character
+ */
+
+char *_strchr(char *s, char c)
+{
+if (c = s[c])
+putchar(c);
+else
+putchar('\0');
+
+return (c);
+}
