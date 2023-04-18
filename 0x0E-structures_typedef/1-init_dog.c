7a2c6740308b984818a2a67dce88b776a5bb08bd
mwambajoakim <mwambajoakim@gmail.com>
2023-04-18 11:09:38 +0300

    A function that initializes struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,11 +15,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d)
+struct dog
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+d->name;
+d->age;
+d->owner;
 return;
 }
