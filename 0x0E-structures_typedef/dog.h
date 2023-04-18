d0946b59177c4443ee1be6ad2e056f2be7bed3db
mwambajoakim <mwambajoakim@gmail.com>
2023-04-18 09:41:37 +0300

    The dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog _ name of the structure
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+*/
+
 struct dog
 {
   char *name;
   float age;
   char *owner;
-}
+};
