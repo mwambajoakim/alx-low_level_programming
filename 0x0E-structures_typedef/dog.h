8bc15d98aacb3f94f63d9c731a10a25364c72186
mwambajoakim <mwambajoakim@gmail.com>
2023-04-18 09:47:24 +0300

    The dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog _ name of the structure
+ * struct dog - name of the structure
  * @name: the dog's name
  * @age: the dog's age
  * @owner: the dog's owner
@@ -10,9 +10,9 @@
 
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
