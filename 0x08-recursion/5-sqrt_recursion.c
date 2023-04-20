3b128a7e2db0df6ff1f07770d33bdb5331c14dbb
mwambajoakim <mwambajoakim@gmail.com>
2023-04-20 09:12:55 +0300

    A function that gives the square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,25 +4,25 @@
  * check - square root
  * @p:int
  * @q:int
- * Return: int
+ * Return: integer
  */
-int sqrt(int p, int q)
+int check(int p, int q)
 {
 	if (p * p == q)
 		return (p);
 	if (p * p > q)
 		return (-1);
-	return (sqrt(p + 1, q));
+	return (check(p + 1, q));
 }
 
 /**
- * _sqrt_recursion - return sq root
- * @n: int
+ * _sqrt_recursion - A function that gives square root
+ * @n: integer to check
  * Return: square root or -1
  */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (sqrt(1, n));
+	return (check(1, n));
 }
