f322a803066619677af6145e37d2142479777b35
mwambajoakim <mwambajoakim@gmail.com>
2023-04-20 12:07:25 +0300

    A function that will sum its arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <variadic_functions.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - function that will sum its arguments
+ * @n: arguments to be summed
+ * Return: an integer
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int d, result = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (d = 0; d < n; d++)
+	{
+	result += va_arg(args, n);
+	}
+	va_end;
+	return (result);
+}
