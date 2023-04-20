6992cea1b1fc7ebc485f6065571a9de6e8b70e42
mwambajoakim <mwambajoakim@gmail.com>
2023-04-20 08:52:35 +0300

    A program that gives the least amount of change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that gives least amount of change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv)
+{
+	unsigned long int i;
+	int cents[] = {25, 10, 5, 2, 1};
+	int total, count;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]), count = 0;
+
+	if (total < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (i = 0; total > 0 && i < sizeof(cents) / sizeof(cents[0]); i++)
+	{
+		count += total / cents[i];
+		total %= cents[i];
+	}
+
+	printf("%d\n", count);
+
+	return (0);
+}
