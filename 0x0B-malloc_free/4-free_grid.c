fd0965adc24c7e4fd4cf6b2608a12e110b2c1f46
mwambajoakim <mwambajoakim@gmail.com>
2023-06-15 16:41:26 +0300

    A function that frees previously allocated memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* free_grid - a function that frees memory
+* for a 2D array previously allocated
+*
+* @grid: pointer to the grid
+* @height: height of grid
+* Return: nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int j;
+
+for (j = 0; j < height; j++)
+	free(grid[j]);
+	free(grid);
+}
