37bbcdcac9305f98eb289114bd9bff346405451c
mwamba <mwambajoakim@gmail.com>
2024-06-12 16:19:04 +0300

    Duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib>
+#include <stdio.h>
+
+
+/**
+ *_strlen - function that returns length of string
+ *
+ *@str: pointer to string
+ *
+ *Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	return (str);
+}
+
+/**
+ *_strdup - function that returns pointer to newly allocated space
+ *
+ *@str: pointer to string
+ *
+ *Return: pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, size = _strlen(str) + 1;
+	char *dup;
+
+	dup = (char *)malloc(size * sizeof(char));
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
+}
