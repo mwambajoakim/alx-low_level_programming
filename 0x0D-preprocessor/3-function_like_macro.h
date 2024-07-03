45b0740992fd7f12bafabbaff5d08d5ecd1d568c
mwamba <mwambajoakim@gmail.com>
2024-07-03 17:43:46 +0300

    define macro ABS

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef 3_function_like_macro_h
+#define 3_function_like_macro_h
+
+#ifndef ABS
+#define ABS(x)
+
+#if x < 0
+(x) * (-1)
+#endif
+
+#endif /* ABS */
+#endif /* 3_function_like_macro_h */
