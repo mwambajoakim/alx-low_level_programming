d0409ff3388c23ce0f59578d34f4b9985f3d0f6d
mwamba <mwambajoakim@gmail.com>
2024-07-08 12:28:11 +0300

    add condition for if NULL is passed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * array_iterator - function that executes a fn given a parameter
@@ -14,8 +15,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
