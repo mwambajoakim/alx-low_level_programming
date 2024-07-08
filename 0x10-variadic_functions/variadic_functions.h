0e68a9ff16d7c1a601bd2b51536d65642f768a86
mwamba <mwambajoakim@gmail.com>
2024-07-08 17:47:37 +0300

    add a header file for the project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigneed int n, ...);
+void print_stringss(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
