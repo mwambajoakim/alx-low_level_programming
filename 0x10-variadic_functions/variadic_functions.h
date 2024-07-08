54b330de96271649077e5a4b6d6b8161a6ce732d
mwamba <mwambajoakim@gmail.com>
2024-07-08 17:56:16 +0300

    change the typo

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigneed int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_stringss(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
