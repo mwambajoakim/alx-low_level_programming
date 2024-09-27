271b319d3a3f123914a11f7e10e4a647232a889d
mwamba <mwambajoakim@gmail.com>
2024-09-27 21:58:33 +0300

    Solve errors and warnings

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ *main - entry point
  *
- *return: 0
+ * Return: 0 is success
  */
 
 int main(void)
 {
-	int a; int b; int c;
+	int a;
+	int b;
+	int c;
 
-  for (a = 0; a <= 7; a++)
-    {
-	    for (b = a; b <= 8; b++)
-	    {
-		    for (c = b; c <= 9; c++)
-		    {
-			    if (a != b && b != c && a != c)
-			    {
-				    putchar(a + '0');
-				    putchar(b + '0');
-				    putchar(c + '0');
-				    if (a != 7 || b != 8 || c != 9 )
-				    {
-					    putchar(',');
-					    putchar(' ');
-				    }
-			    }
-		    }
-	    }
-    }
-  putchar('\n');
-  return (0);
+for (a = 0; a <= 7; a++)
+{
+	for (b = a; b <= 8; b++)
+	{
+		for (c = b; c <= 9; c++)
+		{
+			if (a != b && b != c && a != c)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
 }
