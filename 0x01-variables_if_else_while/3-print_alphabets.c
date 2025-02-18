04dc5e8a882a5c209a83125dfc2e756944119b73
mwamba <mwambajoakim@gmail.com>
2025-02-18 15:44:12 +0300

    Add simple boilerplate and algorithm

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	/*Declare two variables for the characters*/
+
+	/*Loop through lowercase alphabet*/
+		/*Output each character*/
+		/*Go up a character until end*/
+
+	/*Loop through lowercase alphabet*/
+		/*Output each character*/
+		/*Go up a character until end*/
+	
+	return(0);
+}
