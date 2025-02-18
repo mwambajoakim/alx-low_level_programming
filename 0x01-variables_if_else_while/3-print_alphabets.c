fd666ff29b9200282a29b43f3e46471f9a7a95ca
mwamba <mwambajoakim@gmail.com>
2025-02-18 15:52:25 +0300

    print lowercase and uppercase letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,26 @@
 int main(void)
 {
 	/*Declare two variables for the characters*/
+	char l_alphabet = 'a';
+	char u_alphabet = 'A';
 
 	/*Loop through lowercase alphabet*/
+	while (l_alphabet < 'z')
+	{
 		/*Output each character*/
+		putchar(l_alphabet);
 		/*Go up a character until end*/
+		l_alphabet++;
+	}
 
 	/*Loop through lowercase alphabet*/
+	while (u_alphabet < 'Z')
+	{
 		/*Output each character*/
+		putchar(u_alphabet);
 		/*Go up a character until end*/
-	
-	return(0);
+		u_alphabet++;
+	}
+	putchar('\n');
+return (0);
 }
