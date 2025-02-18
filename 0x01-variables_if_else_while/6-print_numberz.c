b91addf94957f8dac1346f4e4c7d29d6084be619
mwamba <mwambajoakim@gmail.com>
2025-02-18 16:37:08 +0300

    Print numbers ascending

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,5 +8,17 @@
 
 int main(void)
 {
-	
+	/*Declare variable to hold first number*/
+	int number = 0;
+
+	/*Loop through the numbers*/
+	while (number <= 9)
+	{
+		/*Output each number*/
+		putchar(number + '0');
+		/*Go down one number until the end*/
+		number++;
+	}
+	putchar('\n');
+return (0);
 }
