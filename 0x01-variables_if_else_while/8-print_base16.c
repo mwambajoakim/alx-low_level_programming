9877f5e3418b642ff52d228f012494348f20669c
mwamba <mwambajoakim@gmail.com>
2025-02-18 16:53:28 +0300

    Print numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,5 +8,23 @@
 
 int main(void)
 {
+	/*Declare two variables for number and letters*/
+	int number = 0;
+	char alphabet = 'a';
 
+	/*Loop through the numbers*/
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	/*Loop through the alphabets until f*/
+	while (alphabet <= 'f')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+return (0);
 }
