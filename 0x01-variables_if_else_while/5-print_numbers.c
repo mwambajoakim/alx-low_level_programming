32a880969108a3f5f52241f958b687a4f4bfd967
mwamba <mwambajoakim@gmail.com>
2025-02-18 16:21:11 +0300

    Print numbers 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,10 +9,16 @@
  int main(void)
 {
 	/*Declare variable to  hold first digit*/
+	int number = 0;
 
 	/*Loop through the numbers*/
+	while (number <= 9)
+	{
 		/*Output each number*/
+		putchar(number + '0');
 		/*Go up one number*/
-
+		number++;
+	}
+	putchar('\n');
 return (0);
 }
