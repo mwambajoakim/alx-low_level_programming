6f43b63e79d8f96589856916ef86448076874677
mwamba <mwambajoakim@gmail.com>
2025-02-18 16:10:31 +0300

    print alphabet lowercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,23 @@
  * Return: 0 is success
  */
 
- int main(void)
+int main(void)
 {
 	/*Declare variable to hold character*/
+	char l_alphabet = 'a';
 
 	/*Loop through alphabet*/
+	while (l_alphabet <= 'z')
+	{
 		/*Exclude 'q' and 'e'*/
+		if (l_alphabet != 'e' && l_alphabet != 'q')
+		{
 			/*Output each character*/
+			putchar(l_alphabet);
+		}
 		/*Go up ome character until end*/
-retirn (0);
+		l_alphabet++;
+	}
+	putchar('\n');
+return (0);
 }
