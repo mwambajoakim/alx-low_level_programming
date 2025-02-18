f20b8ae94c081d8cac605bbce1ae86678a51f47e
mwamba <mwambajoakim@gmail.com>
2025-02-18 16:18:19 +0300

    Write algorithm

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,7 +7,12 @@
  */
 
  int main(void)
- {
-	
+{
+	/*Declare variable to  hold first digit*/
+
+	/*Loop through the numbers*/
+		/*Output each number*/
+		/*Go up one number*/
+
 return (0);
- }
+}
