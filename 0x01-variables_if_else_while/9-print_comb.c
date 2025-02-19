892ac176de6af01ff617eab1234db5387074da99
mwamba <mwambajoakim@gmail.com>
2025-02-19 10:57:25 +0300

    Print comma-separated single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,8 +6,29 @@
  * Return: 0 is success
  */
 
- int main(void)
- {
+int main(void)
+{
+	/*Declare variable to hold number*/
+	int number = 0;
 
- }
- 
+	/*Loop through the numbers*/
+	while (number <= 9)
+	{
+		/*If the number is not equal to 9*/
+		if (number < 9)
+		{
+			/*Output the number, a comma and a space*/
+			putchar(number + '0');
+			putchar(',');
+			putchar(' ');
+		}
+
+		else
+		{
+			putchar(number + '0');
+		}
+		/*Go up one number at a time*/
+		number++;
+	}
+return (0);
+}
