feb206e11664f7c5292eab675d671ad091f9496b
mwamba <mwambajoakim@gmail.com>
2025-02-19 11:43:16 +0300

    Use four putchars

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,18 +14,15 @@ int main(void)
 	/*Loop through the numbers*/
 	while (number <= 9)
 	{
+		putchar(number + '0');
 		/*If the number is not equal to 9*/
 		if (number != 9)
 		{
 			/*Output the number, a comma and a space*/
-			putchar(number + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(number + '0');
-		}
+
 		/*Go up one number at a time*/
 		number++;
 	}
