2b9e3ce6cd3831700b59a64a1a87e0eb4755d0c9
mwamba <mwambajoakim@gmail.com>
2025-02-20 16:01:31 +0300

    Compute integer absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,5 +10,12 @@
 
 int _abs(int number)
 {
-
+	/*If less than zero multiply by -1 return abs-value*/
+	if (number < 0)
+	{
+		number *= -1;
+		return (number);
+	}
+	/*Return number otherwise*/
+	return (number);
 }
