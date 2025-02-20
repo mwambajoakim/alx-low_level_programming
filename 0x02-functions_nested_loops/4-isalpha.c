e714bea04328b99c4d56dd3c89202781610f2d95
mwamba <mwambajoakim@gmail.com>
2025-02-20 15:13:33 +0300

    Add conditions to check for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,15 @@
  *
  * @c: character to check
  *
- * Return: 1 if it exists 0 if otherwise 
+ * Return: 1 if it exists 0 if otherwise
  */
 
 int _isalpha(int c)
 {
-	
+	/*If alphabet exists return 1*/
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	/*Return 0 if otherwise*/
+	else
+		return (0);
 }
