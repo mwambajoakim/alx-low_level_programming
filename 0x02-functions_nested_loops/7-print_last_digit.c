165316d7707a3722758f602d9e2160bb30bb24e6
mwamba <mwambajoakim@gmail.com>
2025-02-20 17:48:00 +0300

    Add function boilerplate

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @number: integer to print last number
+ *
+ * Return: Last number
+ */
+
+int print_last_digit(int number)
+{
+
+}
