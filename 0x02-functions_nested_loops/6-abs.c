5af7611c34022ddbbb6d0c0c2801a8db8682d1de
mwamba <mwambajoakim@gmail.com>
2025-02-20 15:55:52 +0300

    Add function boilerplate

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of integer.
+ *
+ * @number: integer to return absolute value.
+ *
+ * Return: absolute value.
+ */
+
+int _abs(int number)
+{
+
+}
