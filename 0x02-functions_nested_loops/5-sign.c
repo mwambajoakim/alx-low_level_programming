2145abc7d551a1452ab52815309fb8f331914a7c
mwamba <mwambajoakim@gmail.com>
2025-02-20 15:19:22 +0300

    Add function boilerplate

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for sign
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if zero, 0 if 0 and -1 if negative
+ */
+
+int print_sign(int n)
+{
+
+}
