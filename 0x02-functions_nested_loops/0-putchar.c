53fea7a0ecd18906bccfc88aff6ffffd42d16426
mwamba <mwambajoakim@gmail.com>
2025-02-20 13:28:33 +0300

    Define _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar -entry point function
+ *
+ * @h: character to stdout
+ * Return: 1 is success
+ */
+
+int _putchar(char h)
+{
+return (write(1, &h, 1));
+}
 
 /**
  * main -entry point function
@@ -8,6 +20,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }
