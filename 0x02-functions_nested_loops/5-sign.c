44c9e55dc746fd11cf3c681d800801fb6ad56b09
mwamba <mwambajoakim@gmail.com>
2025-02-20 15:29:55 +0300

    Add conditions to check for signage

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,30 @@
 #include "main.h"
 
 /**
- * print_sign - checks for sign
+ * print_sign - checks for sign.
  *
- * @n: number to be checked
+ * @n: number to be checked.
  *
- * Return: 1 if zero, 0 if 0 and -1 if negative
+ * Return: 1 if positive, 0 if 0 and -1 if negative.
  */
 
 int print_sign(int n)
 {
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
 
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
