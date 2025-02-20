6ba8ecf1e59704b1cce1bdbc08d6f303fb96e678
mwamba <mwambajoakim@gmail.com>
2025-02-20 14:20:19 +0300

    Add simple function boilerplate

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks if alphabet is lowercase
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase 0 otherwise
+ */
+
+int _islower(int c)
+{
+
+}
