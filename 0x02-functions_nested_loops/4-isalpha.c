bae946b6e80b0e42ce35fdf87bf04b544c4cbc06
mwamba <mwambajoakim@gmail.com>
2025-02-20 15:07:40 +0300

    Add function boilerplate

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: character to check
+ *
+ * Return: 1 if it exists 0 if otherwise 
+ */
+
+int _isalpha(int c)
+{
+	
+}
