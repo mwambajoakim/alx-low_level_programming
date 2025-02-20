f8635ef2f39c54d86991e340954d7575f0840126
mwamba <mwambajoakim@gmail.com>
2025-02-20 14:06:29 +0300

    Print alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,18 @@
 void print_alphabet_x10(void)
 {
 	/*Define two variables*/
+	int table;
+	char l_alphabet;
 
 	/*Create a table to print the alphabets*/
+	for (table = 1; table <= 9; table++)
+	{
 		/*Loop through the alphabet*/
+		for (l_alphabet = 'a'; l_alphabet <= 'z'; l_alphabet++)
+		{
 			/*Print characters to stdout*/
-
+			_putchar(l_alphabet);
+		}
+	}
+	_putchar('\n');
 }
