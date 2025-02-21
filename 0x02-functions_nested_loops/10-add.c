db0e48ef1c84db4e62e5adddffa8e919107e89c3
mwamba <mwambajoakim@gmail.com>
2025-02-21 16:15:08 +0300

    Sum two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,10 +6,10 @@
  * @number1: First number to add.
  * @number2: econd number to add.
  *
- * Return: um of the two numbers.
+ * Return: Sum of the two numbers.
  */
 
 int add(int number1, int number2)
 {
-
+return (number1 + number2);
 }
