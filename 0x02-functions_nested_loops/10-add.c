a04e4d5afa38704665fad80d632d27924d1833ba
mwamba <mwambajoakim@gmail.com>
2025-02-21 16:13:01 +0300

    Add function boilerplate

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -Adds two numbers.
+ *
+ * @number1: First number to add.
+ * @number2: econd number to add.
+ *
+ * Return: um of the two numbers.
+ */
+
+int add(int number1, int number2)
+{
+
+}
