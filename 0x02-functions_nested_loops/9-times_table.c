2013fc2bc40e2c03f8d96eeaf4d026764aed9c86
mwamba <mwambajoakim@gmail.com>
2025-02-21 15:53:27 +0300

    Create nine times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,28 @@
  */
 
 void times_table(void)
-[
+{
+	int first, second, mult = 0;
 
-]
+	for (first = 0; first <= 9; first++)
+	{
+		for (second = 0; second <= 9; second++)
+		{
+			mult = first * second;
+			if (second == 9)
+			{
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+
+			else
+			{
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
