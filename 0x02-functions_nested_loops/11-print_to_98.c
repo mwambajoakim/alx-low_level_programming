85a01a91477a82ea0b570c8aa4235acefe0dbfdb
mwamba <mwambajoakim@gmail.com>
2025-02-22 20:11:11 +0300

    Add conditionals to print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,5 +10,42 @@
 
 void print_to_98(int n)
 {
+	int end = 98;
 
+	if (n == end)
+	{
+		printf("%d", end);
+	}
+
+	else
+	{
+		while (end < n)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+
+			else
+			{
+				printf("%d", n);
+			}
+			n--;
+		}
+
+		while (n <= end)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
+		}
+	}
+	printf("\n");
 }
