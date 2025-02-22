15001cb3205faac19136ac9d8d482063b0bb7682
mwamba <mwambajoakim@gmail.com>
2025-02-22 22:30:32 +0300

    Add function boilerplate

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * largest_number - Prints the largest number of three.
+ *
+ * @a: First number.
+ * @b: Second number.
+ * @c: Third number.
+ *
+ * Return: Largest number.
+ */
+
+int largest_number(int a, int b, int c)
+{
+
+}
