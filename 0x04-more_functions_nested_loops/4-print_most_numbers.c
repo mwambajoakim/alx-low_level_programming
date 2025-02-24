5d4c117b017be4bfdaba6c6f03e2fc714e0b9ba0
mwamba <mwambajoakim@gmail.com>
2025-02-24 20:03:51 +0300

    Print 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,4 +8,14 @@
 
 void print_most_numbers(void)
 {
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		if (!(numbers == 2 && numbers == 4))
+		{
+			_putchar(numbers + '0');
+		}
+	}
+	_putchar('\n');
 }
