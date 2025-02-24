5fe732831c4e479a630779b90daffb0b13d2a887
mwamba <mwambajoakim@gmail.com>
2025-02-24 14:53:33 +0300

    Add condition to increment day by one in non-leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,6 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
+		day++;
 		if (month >= 2 && day >= 60)
 		{
 			printf("Day of the year: %d\n", day);
@@ -20,7 +21,6 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			day++;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
@@ -33,7 +33,6 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			day--;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
