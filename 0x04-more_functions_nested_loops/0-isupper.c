cddc5157103513c01eaa481cb8284e0ba4c97bbb
mwamba <mwambajoakim@gmail.com>
2025-02-24 18:53:15 +0300

    Add function boilerplate

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ *
+ * @c: Character to be checked.
+ *
+ * Return: 1 if uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+
+}
