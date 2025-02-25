1e165b3472c212fe4387b56b74a6b94236332c9e
mwamba <mwambajoakim@gmail.com>
2025-02-25 15:41:34 +0300

    Loop through numbers 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,5 +8,15 @@
 
 void more_numbers(void)
 {
+	int rows_10;
+	int numbers;
 
+	for (rows_10 = 0; rows_10 <= 9; rows_10++)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			_putchar(numbers + '0');
+		}
+	}
+	_putchar('\n');
 }
