06b896913d8c2b36ef6a5c31731e2ee4db2aa879
mwamba <mwambajoakim@gmail.com>
2025-02-26 21:06:37 +0300

    Print square onto stdout

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,5 +10,21 @@
 
 void print_square(int size)
 {
+	int space, start;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (space = 0; space < size; space++)
+		{
+			for (start = 0; start < size; start++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
