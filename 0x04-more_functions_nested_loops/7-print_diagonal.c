0823f2bb1372037a4f47dd513b24def25b5fbdbd
mwamba <mwambajoakim@gmail.com>
2025-02-26 20:55:11 +0300

    Print diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,5 +10,24 @@
 
 void print_diagonal(int n)
 {
+	/*pace is for rows and start for columns*/
+	int space, start;
 
+	/*When n is 0 or negative just put a new line*/
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (space = 0; space < n; space++)
+		{
+			for (start = 0; start < space; start++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
