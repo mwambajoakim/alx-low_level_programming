b929deb00a2ee2dbde3c506cc6567f984853f21a
mwamba <mwambajoakim@gmail.com>
2025-02-26 21:25:14 +0300

    Define function to print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,5 +8,28 @@
 
 int main(void)
 {
+	int numbers;
 
+	for (numbers = 0; numbers <= 100; numbers++)
+	{
+		if (numbers / 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (numbers / 5 == 0)
+
+		{
+			printf("Buzz ");
+		}
+		else if ((numbers / 3 == 0) && (numbers / 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", numbers);
+		}
+	}
+	printf("\n)");
+return (0);
 }
