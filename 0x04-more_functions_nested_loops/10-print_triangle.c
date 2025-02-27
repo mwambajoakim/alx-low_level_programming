af8c7eed2ace6a0cb4087fdd3e85c53a3db6e796
mwamba <mwambajoakim@gmail.com>
2025-02-27 16:04:56 +0300

    Add a function description

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,15 @@
 
 void print_size(int size)
 {
+	int space, start;
 
+	for (space = 0; space < size; space++)
+	{
+		for (start = 0; start < size; start++)
+		{
+			_putchar(" ");
+		}
+		_putchar("#");
+		_putchar('\n');
+	}
 }
