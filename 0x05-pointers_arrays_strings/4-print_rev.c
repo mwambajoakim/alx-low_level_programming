a127f9f42c64543292ee74023df99e5283200291
mwamba <mwambajoakim@gmail.com>
2025-03-02 08:28:19 +0300

    Add function t calculate length of a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to string.
+ *
+ * Return: Length of the string s.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
  * print_rev - Prints a string pointed to by s in reverse.
  *
  * @s: Pointer to string.
