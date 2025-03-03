43381eb460ab223e51da82cb52d3d342cee97dc4
mwamba <mwambajoakim@gmail.com>
2025-03-03 23:12:56 +0300

    Define string length function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to the string.
+ *
+ * Return: Length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+}
+
+/**
  * puts_half - Prints half of a string, then a new line.
  *
  * @str: Pointer to the string.
