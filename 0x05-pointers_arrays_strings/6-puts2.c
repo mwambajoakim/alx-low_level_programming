4640f152260e7ca3fd40c8fc09f5b33f27e0ed2e
mwamba <mwambajoakim@gmail.com>
2025-03-03 22:45:41 +0300

    Define function to get string length

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to the string.
+ *
+ * Return: Length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
  * puts2 - Prints every other character of a string
  * staring with the first character.
  *
@@ -9,5 +29,7 @@
 
 void puts2(char *str)
 {
+	int i;
 
+	for ()
 }
