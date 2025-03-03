f5c050ec85ff5472ae3e7b4fe72c51a7c489e4d8
mwamba <mwambajoakim@gmail.com>
2025-03-03 22:36:56 +0300

    Swap characters in a string to reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to string.
+ *
+ * Return: Length of the string s.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
  * rev_string - Reverses a string pointed to by s.
  *
  * @s: Pointer to a string.
@@ -8,5 +28,18 @@
 
 void rev_string(char *s)
 {
+	char *new = s;
+	char *final, temp;
 
+	final = s + _strlen(s) - 1;
+
+	while (new < final)
+	{
+		temp = *new;
+		*new = *final;
+		*final = temp;
+
+		new++;
+		final--;
+	}
 }
