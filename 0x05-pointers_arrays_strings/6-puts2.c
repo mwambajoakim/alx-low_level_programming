425bc765e010246cde193145a26c59064fad685e
mwamba <mwambajoakim@gmail.com>
2025-03-03 22:49:56 +0300

    Remove string length function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- *
- * @s: Pointer to the string.
- *
- * Return: Length of the string.
- */
-
-int _strlen(char *s)
-{
-	int i;
-	int length = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length++;
-	}
-	return (length);
-}
-
-/**
  * puts2 - Prints every other character of a string
  * staring with the first character.
  *
@@ -31,5 +11,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for ()
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
