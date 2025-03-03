25b958d76e7d073030e22128814b6f1492b127f9
mwamba <mwambajoakim@gmail.com>
2025-03-03 22:41:22 +0300

    Write function description

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * staring with the first character.
+ *
+ * @str: Pointer to string.
+ */
+
+void puts2(char *str)
+{
+
+}
