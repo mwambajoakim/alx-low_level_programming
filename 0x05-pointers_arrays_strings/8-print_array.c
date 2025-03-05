dd0f7e7eb02d1601ecf4959703b4607448410c05
mwamba <mwambajoakim@gmail.com>
2025-03-05 09:33:26 +0300

    Write function description

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_array - Prints the elemts of an array.
+ *
+ * @a: Pointer to array of elements.
+ * @n: Number of elements in the array.
+ */
+
+void print_array(int *a, int n)
+{
+
+}
