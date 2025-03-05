acea2244785409d44e1ce6f96b33e8f7c4196368
mwamba <mwambajoakim@gmail.com>
2025-03-05 10:00:28 +0300

    Copy string src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,5 +13,15 @@
 
 char *strcpy(char *dest, char *src)
 {
+	int i;
+	int j;
 
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (j = 0; src[j] <= '\0'; j++)
+		{
+			dest[i] = src[i];
+		}
+	}
+	return (dest);
 }
