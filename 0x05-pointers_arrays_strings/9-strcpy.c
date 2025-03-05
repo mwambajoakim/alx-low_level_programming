242a265680c50dd0f5acd9bec814bde0a3174dd8
mwamba <mwambajoakim@gmail.com>
2025-03-05 09:56:49 +0300

    Write function description

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src, 
+ * including the terminating null byte (\0), to the
+ *  buffer pointed to by dest. 
+ *
+ * @dest: Pointer to string.
+ * @src: Pointer to string.
+ *
+ * Return: Pointer to a character.
+ */
+
+char *strcpy(char *dest, char *src)
+{
+
+}
