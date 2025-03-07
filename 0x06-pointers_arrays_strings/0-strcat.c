69a978b6c8437acf7a1987f434fdda666cb61067
mwamba <mwambajoakim@gmail.com>
2025-03-07 10:41:28 +0300

    Write function description

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ *
+ * @dest: Pointer to string.
+ * @src: Pointer to string.
+ *
+ * Return: Pointer to dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
