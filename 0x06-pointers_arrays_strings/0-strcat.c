3e92f9227a1d5e5ad3d67d5a33f9bb6e323cbb00
mwamba <mwambajoakim@gmail.com>
2025-03-10 15:48:49 +0300

    Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,5 +11,20 @@
 
 char *_strcat(char *dest, char *src)
 {
+	char *temp = dest;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	while (*src != '\0')
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+	*temp = '\0';
+	return (dest);
 
 }
