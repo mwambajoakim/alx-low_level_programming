a1fe193a728609e27f09316da67d27054c01d531
mwamba <mwambajoakim@gmail.com>
2025-03-10 15:55:54 +0300

    Write function description

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings, dest and src.
+ *
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ * @n: Number of src bytes.
+ *
+ * Return: Pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
