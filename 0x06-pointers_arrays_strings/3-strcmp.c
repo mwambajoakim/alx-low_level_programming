494012a3d22798b47b16d9048d1d645339a22438
mwamba <mwambajoakim@gmail.com>
2025-03-11 16:25:01 +0300

    Write function description

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares strings s1 and s2.
+ *
+ * @s1: Pointer to string 1.
+ * @s2: Pointer to string 2.
+ *
+ * Return: Difference between s1 and s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+}
