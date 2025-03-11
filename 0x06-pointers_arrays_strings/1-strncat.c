3a3032be12d1d8af7a7e0fc8a43fa8a19c2dcfd1
mwamba <mwambajoakim@gmail.com>
2025-03-11 14:48:52 +0300

    Concatenate a particular number of bytes to string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,5 +12,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i = n;
+	char *p = dest;
 
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0' && i > 0)
+	{
+		i--;
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
+	return (dest);
 }
