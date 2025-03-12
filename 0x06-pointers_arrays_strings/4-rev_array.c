146bdc575ccc76ac30137f5a9c7232e88d820c55
mwamba <mwambajoakim@gmail.com>
2025-03-12 21:32:31 +0300

    Write function description

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the elements of an array.
+ *
+ * @a: Pointer to an array.
+ * @n: Number of elements of the array.
+ */
+
+void reverse_array(int *a, int n)
+{}
