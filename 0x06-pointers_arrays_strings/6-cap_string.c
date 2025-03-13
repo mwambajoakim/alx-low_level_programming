49a1a833e05c341a64d7c2095b5bbfad64ab7431
mwamba <mwambajoakim@gmail.com>
2025-03-13 11:50:06 +0300

    Write function description

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes every first letter
+ * of a word.
+ *
+ * @str: Pointer to string.
+ *
+ * Return: Pointer to the string.
+ */
+
+char *cap_string(char *str)
+{}
