ce6f130ba0e7cca42d38b5791cf984b72221b735
mwamba <mwambajoakim@gmail.com>
2025-03-13 15:18:32 +0300

    Write leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,5 +8,24 @@
  * Return: Pointer to the string.
  */
 
-char *leet(char str)
-{}
+char *leet(char *str)
+{
+	int i, j;
+	char leet_letters[] = {'a', 'e', 'o', 'l', 't',
+	'A', 'E', 'O', 'L', 'T'};
+	char leet_nums[] = {'4', '3', '0', '7', '1',
+	'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == leet_letters[j])
+			{
+				str[i] = leet_nums[j];
+			}
+		}
+
+	}
+	return (str);
+}
