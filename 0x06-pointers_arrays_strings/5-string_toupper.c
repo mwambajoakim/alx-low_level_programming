8f6c35328dc2a876bc2c5d6d623119fc1758ae32
mwamba <mwambajoakim@gmail.com>
2025-03-13 11:44:43 +0300

    Return string in uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,4 +8,15 @@
  */
 
 char *string_toupper(char *str)
-{}
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+	}
+	return (str);
+}
