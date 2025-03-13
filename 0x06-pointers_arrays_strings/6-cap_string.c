6e012ce28b86acbb9f59d64c424caaf6fd73f41e
mwamba <mwambajoakim@gmail.com>
2025-03-13 12:00:43 +0300

    Write function description

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,4 +10,21 @@
  */
 
 char *cap_string(char *str)
-{}
+{
+	int i;
+	char mark[] = {' ', '\t', '\n', 
+		',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == mark[i])
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+	}
+	return (str);
+}
