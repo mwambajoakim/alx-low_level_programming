a3b125dda2387d4b1a7b940a8bb1280c74f16671
mwamba <mwambajoakim@gmail.com>
2025-03-18 20:23:22 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - Fills n bytes of b to s.
+ *
+ * @s: Pointer to string.
+ * @b: Values to fill memory area with.
+ * @n: Number of bytes.
+ * 
+ * Return: Pointer to s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
