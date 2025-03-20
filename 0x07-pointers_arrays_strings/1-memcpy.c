13655cd06e03e64fd23a088e4a3069552f43679d
mwamba <mwambajoakim@gmail.com>
2025-03-20 20:48:56 +0300

    Forego the null character at src

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char *start = dest;
 
-	for (i = 0; *src != '\0' && i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*dest = *src;
 		dest++;
