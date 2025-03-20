d6786b68f95885d8e81c63fffe3a77b600dd158a
mwamba <mwambajoakim@gmail.com>
2025-03-20 21:11:27 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates character c in string s.
+ *
+ * @s: Pointer to string.
+ * @c: Character to look for.
+ *
+ * Return: Pointer to first occurrence of the character.
+ */
+
+char *_strchr(char *s, char c)
+{}
