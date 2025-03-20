68dc8e3686340cfe3b3ec8408161b336f90fb4b4
mwamba <mwambajoakim@gmail.com>
2025-03-20 20:32:49 +0300

    Copy n bytes to a buffer from source

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,5 +12,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
+	char *start = dest;
 
+	for (i = 0; *src != '\0' && i < n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (start);
 }
