6e9c09dee605fbb4799791fd714c368904d07952
mwamba <mwambajoakim@gmail.com>
2025-03-20 20:23:00 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes of src to dest.
+ *
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ * @n: Number of bytes to copy.
+ *
+ * Return: Pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
