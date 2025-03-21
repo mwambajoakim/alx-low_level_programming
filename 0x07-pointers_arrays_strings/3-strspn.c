847e45b9bf8fc53dd5cbf67d63658305ec8e5abb
mwamba <mwambajoakim@gmail.com>
2025-03-21 22:14:55 +0300

    Remove strlen function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Counts characters in a string.
- *
- * @str: Pointer to a string.
- *
- * Return: Length of string.
- */
-
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-return (len);
-}
-
-/**
  * _strspn - Gets the length of a prefix substring.
  *
  * @s: Pointer to main string.
