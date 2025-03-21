636f047298fb42d3642a4ba5f804cb4c4e633bb7
mwamba <mwambajoakim@gmail.com>
2025-03-21 22:32:40 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Check for any set of bytes.
+ *
+ * @s: Pointer to string.
+ * @accept: Pointer to string of bytes.
+ *
+ * Return: Pointer to first occurrence of bytes.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{}
