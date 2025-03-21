83efb28be441a26957198e708685a843afe3695a
mwamba <mwambajoakim@gmail.com>
2025-03-21 15:00:33 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ *
+ * @s: Pointer to main string.
+ * @accept: Pointer to prefix substring.
+ *
+ * Return:  Length of prefix substring.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{}
