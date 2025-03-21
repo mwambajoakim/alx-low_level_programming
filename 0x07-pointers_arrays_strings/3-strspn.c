186855e4ee8afc95897dd80fbc987054997ecf5f
mwamba <mwambajoakim@gmail.com>
2025-03-21 22:12:10 +0300

    Count number of bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -30,4 +30,24 @@ return (len);
  */
 
 unsigned int _strspn(char *s, char *accept)
-{}
+{
+	unsigned int count = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				break;
+			}
+		}
+		if (!accept[i])
+		{
+			break;
+		}
+	}
+	return (count);
+}
