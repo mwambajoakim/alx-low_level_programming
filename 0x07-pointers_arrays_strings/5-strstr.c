f43bf73aef571321ad9a4429557f4ad7e0bc3b54
mwamba <mwambajoakim@gmail.com>
2025-03-21 22:44:40 +0300

    Write function description

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - Gets the first occurence of needle in haystack.
+ *
+ * @haystack: Pinter to string to look.
+ * @needle: Pointer to string of occurence to look for.
+ *
+ * Return: Pointer to first occurence of needle in haystack.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{}
