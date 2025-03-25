10af1c422f1d732e78c456645efa9abe7988fbbf
mwamba <mwambajoakim@gmail.com>
2025-03-25 18:47:03 +0300

    Define puts function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _puts_recursion - Prints string s.
+ *
+ * @s: Pointer to string.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+/**
  * _print_rev_recursion - Prints string s in reverse.
  *
  * @s: Pointer to string.
