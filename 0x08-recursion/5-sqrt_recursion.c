5d4a3dad9fb3c40833df8199a206c100dcee3cf2
mwamba <mwambajoakim@gmail.com>
2025-03-26 16:46:31 +0300

    Write function description

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Calculates the sqyare root of n.
+ *
+ * @n: Number to calculate square root.
+ *
+ * Return quare root of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+
+}
