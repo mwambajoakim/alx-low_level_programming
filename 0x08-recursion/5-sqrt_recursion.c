b9f07019c07400d9a302ca27c8a7f3c887468865
mwamba <mwambajoakim@gmail.com>
2025-03-26 17:33:43 +0300

    Add helper function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * sqrt_guess - Guess the square root of a perfect square.
+ *
+ * @n: Number to be checked.
+ * @guess: Random guess.
+ *
+ * Return: Correct guess.
+ */
+
+int sqrt_guess(int n, int guess)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (guess * guess > n)
+	{
+		return (-1)
+	}
+
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+
+	return (_sqrt_guess(n, guess + 1));
+}
+/**
  * _sqrt_recursion - Calculates the sqyare root of n.
  *
  * @n: Number to calculate square root.
@@ -10,5 +38,20 @@
 
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return (sqrt_guess(n, 1));
 }
