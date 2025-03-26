ea5e760e2289464f12c36f9592294e0bb94938d6
mwamba <mwambajoakim@gmail.com>
2025-03-26 16:10:08 +0300

    Write function description

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates number x raised to y.
+ *
+ * @x: Base number.
+ * @y: Power number.
+ *
+ * Return: Result of x to power y.
+ */
+
+int _pow_recursion(int x, int y)
+{}
