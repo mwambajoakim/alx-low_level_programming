76ded7569e10a9931591a4d3ce0574542e0740aa
mwamba <mwambajoakim@gmail.com>
2025-03-26 15:56:24 +0300

    Write function description

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - Returns thee factorial of a given number.
+ *
+ * @n: Number used to calculate factorial.
+ *
+ * Return: Factorial result.
+ */
+
+int factorial(int n)
+{
+
+}
