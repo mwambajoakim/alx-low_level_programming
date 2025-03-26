83f250ce791fe5f6643c06ea4fa289b27f4694f9
mwamba <mwambajoakim@gmail.com>
2025-03-26 18:11:00 +0300

    Write function description

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks if n is a prime number.
+ *
+ * @n: Number to check.
+ *
+ * Return: 1 if prime 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{}
