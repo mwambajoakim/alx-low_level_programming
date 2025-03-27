1533ef32b1df261df692f347a5ebbd1651d244bb
mwamba <mwambajoakim@gmail.com>
2025-03-27 16:30:25 +0300

    Write function description

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry oint function.
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 is success.
+ */
+
+int main(it argc, char *argv[])
+{
+
+}
