8a6bebb05452630557423dbf5ff6f1213558d579
mwamba <mwambajoakim@gmail.com>
2025-03-27 16:42:38 +0300

    Write function description

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point function.
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return 0 is succes, 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
