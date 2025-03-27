32b48acfbaea6d18be5ce12b389ca13ddfc3c396
mwamba <mwambajoakim@gmail.com>
2025-03-27 18:55:28 +0300

    Change string to number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point function.
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			mult *= argv[i];
+			mult *= atoi(argv[i]);
 		}
 	}
 	printf("%d\n", mult);
