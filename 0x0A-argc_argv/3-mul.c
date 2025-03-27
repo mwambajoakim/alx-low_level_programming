1a09fdd5e8caf65a867507cd67c9390d07623c25
mwamba <mwambajoakim@gmail.com>
2025-03-27 18:54:22 +0300

    Multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,10 +6,26 @@
  * @argc: Argument count.
  * @argv: Argument vector.
  *
- * Return 0 is succes, 1 otherwise.
+ * Return: 0 is succes, 1 otherwise.
  */
 
 int main(int argc, char *argv[])
 {
+	int i;
 
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult *= argv[i];
+		}
+	}
+	printf("%d\n", mult);
+	return (0);
 }
