0891ef1f1e32debe6e6043cf477809b332bf5e02
mwamba <mwambajoakim@gmail.com>
2025-03-27 19:37:02 +0300

    Multiply only two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,13 +14,13 @@ int main(int argc, char *argv[])
 {
 	int i, mult = 1;
 
-	if (argc <= 1)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	if (argc > 1)
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
