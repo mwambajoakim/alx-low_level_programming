5def8106fb57ce09d184837ebfe1abb89580423c
mwamba <mwambajoakim@gmail.com>
2025-03-27 16:00:47 +0300

    Write function description

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point function.
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 is success.
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
