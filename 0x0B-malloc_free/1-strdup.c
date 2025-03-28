79b3929a0b5b5526c31fda11588da49b6ccf0bba
mwamba <mwambajoakim@gmail.com>
2025-03-28 20:42:08 +0300

    Add function to get string length

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - Checks for length of a string.
+ *
+ * @str: Pointer to string.
+ *
+ * Return: Length of string str.
+ */
+int _strlen(char *str)
+{
+	int i, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	return (len);
+}
+
+/**
  * _strdup - Points to a newly allocated space in memory
  * containing the string that is a duplicate of str.
  *
@@ -8,8 +24,17 @@
  *
  * Return: Pointer to string.
  */
-
 char *_strdup(char *str)
 {
+	char *dup;
+	int i, length = _strlen(str) + 1;
 
+	dup = malloc(sizeof(char) * length);
+	if (dup == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+		dup[i] = str[i];
+	return (dup);
 }
