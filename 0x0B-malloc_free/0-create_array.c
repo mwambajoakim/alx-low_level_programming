f31289fb6a468f977bd1ce642ef83d1775e9feab
mwamba <mwambajoakim@gmail.com>
2025-03-28 18:32:42 +0300

    Write function description

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * create_array - Creates an array of chars
+ * and initializes it with a specific char.
+ *
+ * @size: Size of array.
+ * @c: Character to fill array with.
+ *
+ * Return: Pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
