2b44bb7bbcffe8012af96eb3f208fcace7112a08
mwamba <mwambajoakim@gmail.com>
2025-03-28 10:26:34 +0300

    Add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
  * main - Entry point function.
  *
@@ -12,5 +12,25 @@
 
 int main(int argc, char *argv[])
 {
+	int i, sum = 0;
 
+	if (argc <= 2)
+	{
+		printf("%d\n", 0);
+	}
+
+	if (argc > 2)
+	{
+		for (i = 2; i < argc; i++)
+		{
+			if (!is_number(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
