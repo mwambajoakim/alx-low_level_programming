8f301b2ba070e53ca721cafc67f74dd5e087284f
mwamba <mwambajoakim@gmail.com>
2025-03-28 18:49:43 +0300

    Write function description

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strdup - Points to a newly allocated space in memory
+ * containing the string that is a duplicate of str.
+ *
+ * @str: Pointer to string.
+ *
+ * Return: Pointer to string.
+ */
+
+char *_strdup(char *str)
+{
+
+}
