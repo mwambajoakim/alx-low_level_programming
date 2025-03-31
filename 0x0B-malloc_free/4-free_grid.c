be6772e3bb8dc53e3cc9b4d4cdae5b484b403e22
mwamba <mwambajoakim@gmail.com>
2025-03-31 18:19:36 +0300

    Include header file properly

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,4 @@
-#imclude "main.h"
+#include "main.h"
 
 /**
  * free_grid - Frees memory allocated to a 2D array.
@@ -9,7 +9,7 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height, i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
