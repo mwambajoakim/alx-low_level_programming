51e614acfd2bfd5315e494448ccb48092401a507
mwamba <mwambajoakim@gmail.com>
2025-03-31 18:07:32 +0300

    Write function description

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,9 @@
+#imclude "main.h"
+
+/**
+ * free_grid - Frees memory allocated to a 2D array.
+ * @grid: Pointer to a 2D array.
+ * @height: Height of the grid.
+ */
+void free_grid(int **grid, int height)
+{}
