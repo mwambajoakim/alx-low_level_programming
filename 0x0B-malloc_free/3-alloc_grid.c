ba8c750bb3798c6b110be8f86e93c97c7253b698
mwamba <mwambajoakim@gmail.com>
2025-03-31 17:02:55 +0300

    Write function description

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates memory to a 2D array.#
+ * @width: Width of array.
+ * @height: Height of array.
+ *
+ * Return: Pointer to the momry address allocated.
+ */
+int **alloc_grid(int width, int height)
+{}
