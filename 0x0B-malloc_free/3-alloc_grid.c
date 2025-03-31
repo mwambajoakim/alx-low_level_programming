4a633f677539a93db210e8a1e7664f02094a9960
mwamba <mwambajoakim@gmail.com>
2025-03-31 17:38:04 +0300

    Allocate memory to an array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,4 +8,31 @@
  * Return: Pointer to the momry address allocated.
  */
 int **alloc_grid(int width, int height)
-{}
+{
+	int i, j, **alloc_mem;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	alloc_mem = malloc(height * sizeof(int));
+	if (alloc_mem == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		alloc_mem[i] = malloc(width * sizeof(int));
+		if (alloc_mem[i] == NULL)
+		{
+		return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			alloc_mem[i][j] = 0;
+		}
+	}
+	return (alloc_mem);
+}
