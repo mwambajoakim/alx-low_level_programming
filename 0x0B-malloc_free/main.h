0e185b49710772e2d40876418e8689656c8c0ea0
mwamba <mwambajoakim@gmail.com>
2025-03-31 16:17:43 +0300

    Add function prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
