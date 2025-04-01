1813504062c78896a864d2003a08cfdcfb21e28a
mwamba <mwambajoakim@gmail.com>
2025-04-02 00:03:22 +0300

    Concatenate n bytes of src to dest in _strncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * _strncat - Concatenates n bytes of src to dest
+ * @dest: Pointer to string.
+ * @src: Pointer to string.
+ * @n: Number of bytes.
+ *
+ * Return: Pointer to dest.
+ */
+char *_strncat(char *dest, char *src, unsined int n)
+{
+	char *tmp = dest;
+	int i = 0;
+
+	while (*tmp != '\0')
+	{
+		tmp++;
+	}
+	while (i < n)
+	{
+		*tmp = *src;
+		tmp++;
+		src++;
+		i++;
+	}
+	*tmp = '\0';
+	return (dest);
+}
+
+/**
  * string_nconcat - Concatenates s1 with n bytes of s2.
  * @s1: Pointer to string.
  * @s2: Ponter to string.
