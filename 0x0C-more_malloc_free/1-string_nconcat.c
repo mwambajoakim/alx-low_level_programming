b8e9ae16757c08ec8da285dcc5fda98a2bcb4c22
mwamba <mwambajoakim@gmail.com>
2025-04-01 23:57:15 +0300

    Describe working of function

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates s1 with n bytes of s2.
+ * @s1: Pointer to string.
+ * @s2: Ponter to string.
+ * @n: Number of bytes of s2.
+ *
+ * Return s1 after the concatenation..
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{}
