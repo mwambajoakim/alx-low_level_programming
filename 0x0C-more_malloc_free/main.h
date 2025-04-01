7efd7b876f3238f7d0c0821683275a668e524cf7
mwamba <mwambajoakim@gmail.com>
2025-04-02 00:16:12 +0300

    Add function prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,8 @@
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, unsigned int n);
+int _strlen(char *str);
 
 #endif /*MAIN_H*/
