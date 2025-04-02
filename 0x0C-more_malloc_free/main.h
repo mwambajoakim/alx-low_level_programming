58c728667b852245ad5b787908d579fc163ba662
mwamba <mwambajoakim@gmail.com>
2025-04-02 20:40:37 +0300

    Add function prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *_strcpy(char *dest, char *src);
 char *_strncat(char *dest, char *src, unsigned int n);
 int _strlen(char *str);
+char *_memset(char *ptr, int c, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /*MAIN_H*/
