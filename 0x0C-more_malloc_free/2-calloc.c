1900aa937c10ef93240c2a7c702844bbd4cbd02b
mwamba <mwambajoakim@gmail.com>
2025-04-02 19:56:37 +0300

    Describe how function works

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory fr an array using malloc.
+ * @nmemb: Number of members in array.
+ * @size: ize of the array.
+ *
+ * Return: Pointer to memory location.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{}
