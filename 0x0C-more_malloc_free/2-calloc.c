8b968b59e4e5a3fbdbec5a7673aa05e50d7ca255
mwamba <mwambajoakim@gmail.com>
2025-04-02 20:38:25 +0300

    Resolve syntax issues

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -34,12 +34,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
-	if (nmemb = 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	ptr = malloc (nmemb * size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
