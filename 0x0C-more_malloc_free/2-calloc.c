16a4b53739d7f1d942550f7ddc9ca9ca34c5ed55
mwamba <mwambajoakim@gmail.com>
2025-04-02 20:34:59 +0300

    Define memset function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _memset - Fills memory are with a constant byte.
+ * @ptr: Pointer to memory area.
+ * @c: Byte to fill memory area with.
+ * @n: size of byte n used.
+ *
+ * Return: Pointer to memory area.
+ */
+void *_memset(void *ptr, int c, unsigned int n)
+{
+	unsigned int i = 0;
+	void *tmp = ptr;
+
+	while (i < n)
+	{
+		*ptr = c;
+		ptr++;
+		i++;
+	}
+	return (tmp);
+}
+
+/**
  * _calloc - Allocates memory fr an array using malloc.
  * @nmemb: Number of members in array.
  * @size: ize of the array.
@@ -9,4 +31,20 @@
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-{}
+{
+	void *ptr;
+
+	if (nmemb = 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc (nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(ptr, 0, size);
+
+	return (ptr);
+}
