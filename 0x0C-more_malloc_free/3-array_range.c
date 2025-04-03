1bfd557caf8eb311570cb11b7e31d04f59837e2f
mwamba <mwambajoakim@gmail.com>
2025-04-03 14:12:42 +0300

    Create ana array and allocate memory

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,4 +8,27 @@
  * Return: Pointer to the array.
  */
 int *array_range(int min, int max)
-{}
+{
+	int *ptr, *tmp;
+	int i =  min;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc((max - min + 1) * sizeof(*ptr));
+	if(ptr == NULL)
+	{
+		return (NULL);
+	}
+	tmp = ptr;
+	while(i <= max)
+	{
+		*tmp = min;
+		tmp++;
+		min++;
+		i++;
+	}
+	return (ptr);
+}
