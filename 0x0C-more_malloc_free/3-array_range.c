067bc065016fb7a2de523ab1a92437f56ff6271b
mwamba <mwambajoakim@gmail.com>
2025-04-03 13:18:30 +0300

    Describe working of the function

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * array_range - Creates an array of integers.
+ * @min: Lower bound of the array.
+ * @max: Upper bound of the array.
+ *
+ * Return: Pointer to the array.
+ */
+int *array_range(int min, int max)
+{}
