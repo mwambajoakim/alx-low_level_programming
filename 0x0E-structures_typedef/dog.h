00616be28988415919f52142b0ddec5e11c5eb60
mwamba <mwambajoakim@gmail.com>
2025-04-04 13:52:54 +0300

    Declare struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - Describes a dog.
+ * @name: Name of dog.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG_H*/
