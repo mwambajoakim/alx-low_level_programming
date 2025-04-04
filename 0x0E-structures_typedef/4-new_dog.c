8dbc9be7a8e5c8c40f2b9352fb64878d0aec8e37
mwamba <mwambajoakim@gmail.com>
2025-04-04 18:55:58 +0300

    Keep copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,86 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: Dog's name.
- * @age: Dog's age.
- * @owner: Dog's owner.
+ * _strlen - Checks for length of a string.
  *
- * Return: Pointer to struct dog_t.
+ * @str: Pointer to string.
+ *
+ * Return: Length of string str.
  */
-dog_t *new_dog(char *name, float age, char *owner)
+int _strlen(char *str)
 {
-	dog_t *tmp_dog;
+	int i, len = 0;
 
-	tmp_dog = malloc(sizeof(*tmp_dog));
-	if (tmp_dog == NULL)
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	return (len);
+}
+
+/**
+ * _strdup - Points to a newly allocated space in memory
+ * containing the string that is a duplicate of str.
+ *
+ * @str: Pointer to string.
+ *
+ * Return: Pointer to string.
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, length;
+
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	tmp_dog->name = name;
-	tmp_dog->age = age;
-	tmp_dog->owner = owner;
+	length = _strlen(str) + 1;
 
-	return (tmp_dog);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	dup = malloc(sizeof(char) * length);
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
 }
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: Pointer to name.
+ * @age: Age of dog.
+ * @owner: Pointer to owner.
+ *
+ * Return: Pointer to struct dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *tmp_dog = malloc(sizeof(dog_t));
+
+if (!tmp_dog)
+	return (NULL);
+
+tmp_dog->name = name ? strdup(name) : strdup("");
+tmp_dog->owner = owner ? strdup(owner) : strdup("");
+
+if (!tmp_dog->name || !tmp_dog->owner)
+{
+	free(tmp_dog->name);
+	free(tmp_dog->owner);
+	free(tmp_dog);
+	return (NULL);
+}
+
+tmp_dog->age = age;
+return (tmp_dog);
+}
+
