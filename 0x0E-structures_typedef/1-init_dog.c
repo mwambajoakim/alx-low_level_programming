f464054a7619ebca9b83d3e7974eebd01eb7d6e5
mwamba <mwambajoakim@gmail.com>
2025-04-04 14:03:06 +0300

    Describe function working

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * init_dog - Initializes struct dog.
+ * @d: Pointer to struct.
+ * @name: Pointer to name.
+ * @age: Dog's age.
+ * @owner: Pointer to owner.
+ */
+void init_dog(struct dog *d, char *name, int age, char *owner)
+{
+}
