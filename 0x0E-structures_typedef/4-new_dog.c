b9be92e1d06c4c5905e8ad8b8f942347dc747d7b
mwamba <mwambajoakim@gmail.com>
2025-04-04 17:30:39 +0300

    Create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,5 +8,18 @@
  *
  * Return: Pointer to struct dog_t.
  */
-dog_t new_dog(char *name, float age, char *owner)
-{}
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *tmp_dog;
+
+	tmp_dog = malloc(sizeof(*tmp_dog));
+	if (tmp_dog == NULL)
+	{
+		return (NULL);
+	}
+	tmp_dog->name = name;
+	tmp_dog->age = age;
+	tmp_dog->owner = owner;
+
+	return (tmp_dog);
+}
