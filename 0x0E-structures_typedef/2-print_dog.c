a191dbeade73b5abe937292e8ce1b7186376e53c
mwamba <mwambajoakim@gmail.com>
2025-04-04 14:44:01 +0300

    Print a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,4 +5,23 @@
  * @d: Pointer to struct.
  */
 void print_dog(struct dog *d)
-{}
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	if (d->age <= 0.0)
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
