0782c5adaabff6c0639c45cfd0e2e7417f0f781f
mwamba <mwambajoakim@gmail.com>
2025-04-04 17:19:19 +0300

    Describe function working

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ *
+ * Return: Pointer to struct dog_t.
+ */
+dog_t new_dog(char *name, float age, char *owner)
+{}
