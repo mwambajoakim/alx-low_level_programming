250b3da9aa431d92451a13ff3ac2613e0f6103f7
mwamba <mwambajoakim@gmail.com>
2025-04-07 17:39:08 +0300

    Describe function and parameters

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Pointer to string name.
+ * @f: Pointer to function.
+ */
+void print_name(char *name, void (*f)(char *))
+{}
