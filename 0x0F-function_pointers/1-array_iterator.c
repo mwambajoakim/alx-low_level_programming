af0d83e4caa8893c0c5eed03317866157b39bb8c
mwamba <mwambajoakim@gmail.com>
2025-04-07 18:40:27 +0300

    Describe function and parameters

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ * on each element of an array
+ * @array: Pointer to array.
+ * @size: ize of the array.
+ * @action: Pointer to function.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{}
