c6f1b203bab11d4e0974623a01c9426169c2ff12
mwamba <mwambajoakim@gmail.com>
2025-04-07 18:57:16 +0300

    Describe function and parameters

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer and returns its index.
+ * @array: Pointer to array.
+ * @size: Size of the array.
+ * @cmp: Pointer to a function.
+ *
+ * Return: Index of integer for which cmp is not 0,
+ * -1 if size <= 0 and if there is no matching element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{}
