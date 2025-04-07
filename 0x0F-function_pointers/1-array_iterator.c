9a5fefe66beb14e3faaa55866b1d06ef57661ddf
mwamba <mwambajoakim@gmail.com>
2025-04-07 18:46:36 +0300

    Executte functions to print array elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,4 +8,17 @@
  * @action: Pointer to function.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
-{}
+{
+	void (*action_tmp)(int);
+	size_t i;
+
+	if (size <= 0)
+	{
+		return;
+	}
+	action_tmp = action;
+	for (i = 0; i < size; i++)
+	{
+		(*action_tmp)(array[i]);
+	}
+}
