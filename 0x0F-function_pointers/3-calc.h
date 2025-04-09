1faa37a1e8e509b0f261e48b0ff11f230f4ffb21
mwamba <mwambajoakim@gmail.com>
2025-04-09 15:44:41 +0300

    Add type defined struct

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,5 +5,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 
 #endif /*CALC_H*/
