922c2227a1a5a71d14f2382d9575c9861c336518
mwamba <mwambajoakim@gmail.com>
2025-04-09 15:50:58 +0300

    Define addition function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds a to b.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: Addition of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
