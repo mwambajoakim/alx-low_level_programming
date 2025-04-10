6c7d391946989b550639d6852c48a36f1ad20b88
mwamba <mwambajoakim@gmail.com>
2025-04-10 16:08:38 +0300

    Define makefile for calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,8 +2,38 @@
 
 /**
  * main - Entry point function.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
  * Return: 0 is success.
  */
-int main(void)
-{}
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operator = get_op_function(argv[2]);
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((argv[2][0] == "/" || argv[2][0] == "%") && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calc = operator(num1, num2);
+	printf("%d\n", calc);
+
+	return (0);
+}
