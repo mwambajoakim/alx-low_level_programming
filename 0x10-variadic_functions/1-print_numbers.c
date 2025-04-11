412eb54ef44f33454a34952630f6dd80d8b5ac88
mwamba <mwambajoakim@gmail.com>
2025-04-11 16:06:16 +0300

    Describe function working

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers.
+ * @separator: Pointer to separator used.
+ * @n: Number of arguments to print.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{}
