0c1914e32d42d1678c33e900b66435a4b8eaa169
mwamba <mwambajoakim@gmail.com>
2025-04-12 02:42:33 +0300

    Return to main if format is null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,7 +10,6 @@ void print_all(const char * const format, ...)
 	int num;
 	float dec;
 	char const *ptr = format;
-	const char *separator = "";
 	va_list args;
 
 	if (format == NULL)
@@ -19,6 +18,7 @@ void print_all(const char * const format, ...)
 	while (*ptr)
 	{
 		switch (*ptr)
+		{
 			case 'c':
 				ltr = va_arg(args, int);
 				printf("%s%c", separator, ltr);
@@ -41,6 +41,7 @@ void print_all(const char * const format, ...)
 				printf("%s%s", separator, str);
 				separator = ", ";
 				break;
+		}
 		ptr++;
 	}
 	printf("\n");
