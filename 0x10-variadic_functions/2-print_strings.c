6eb9e99abf68b7eef6d82b5453989a15c9eade82
mwamba <mwambajoakim@gmail.com>
2025-04-11 18:48:29 +0300

    Handle empty strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,14 +17,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char *);
+		str = va_arg(strings, char*);
 		if (str == NULL)
 		{
 			str = "nil";
-		}
-		else if (*str == '\0')
-		{
-			str = "(empty)";
 		}
 		printf("%s", str);
 		if (i != n - 1)
