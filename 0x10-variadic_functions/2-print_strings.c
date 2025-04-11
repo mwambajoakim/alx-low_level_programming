daf5f483880b380975707d6fce2b0262812cb766
mwamba <mwambajoakim@gmail.com>
2025-04-11 18:45:16 +0300

    Handle empty strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else if (str == '\0')
 		{
-			str == "(empty)";
+			str = "(empty)";
 		}
 		printf("%s", str);
 		if (i != n - 1)
