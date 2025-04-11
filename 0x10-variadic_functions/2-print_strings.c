4e711c7c2c2b24a123a6e074aab1c55f0d213abe
mwamba <mwambajoakim@gmail.com>
2025-04-11 16:57:21 +0300

    Describe function working

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints variable number of strings.
+ * @separator: Pointer to string of separators.
+ * @n: Number of strings to print.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{}
