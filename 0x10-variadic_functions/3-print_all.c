c586aaddad940c966a0281ba44250a1481ce6474
mwamba <mwambajoakim@gmail.com>
2025-04-12 01:55:11 +0300

    Include separator

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,31 +11,35 @@ void print_all(const char * const format, ...)
 	float dec;
 	char *str;
 	char const *ptr = format;
+	char *separator = {", "};
 	va_list args;
 
 	va_start(args, format);
 	while (*ptr)
 	{
-		switch(*ptr)
+		if (*ptr != '\0')
 		{
-			case 'c':
-				ltr = va_arg(args, int);
-				printf("%c", ltr);
-				break;
-			case 'i':
-				num = va_arg(args, int);
-				printf("%d", num);
-				break;
-			case 'f':
-				dec = va_arg(args, double);
-				printf("%f", dec);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "nil";
-				printf("%s", str);
-				break;
+			switch(*ptr)
+			{
+				case 'c':
+					ltr = va_arg(args, int);
+					printf("%c", ltr);
+					break;
+				case 'i':
+					num = va_arg(args, int);
+					printf("%d", num);
+					break;
+				case 'f':
+					dec = va_arg(args, double);
+					printf("%f", dec);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "nil";
+					printf("%s", str);
+					break;
+			}
 		}
 	}
 	printf("\n");
