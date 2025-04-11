d29fc9d2fc6099d825edd11a1361f9cfbf68941b
mwamba <mwambajoakim@gmail.com>
2025-04-11 15:41:29 +0300

    Describe function workiing

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Sums all arguments it is given.
+ * @n: Number of arguments it receives.
+ *
+ * Return: Sum of the arguments.
+ */
+int sum_them_all(const unsigned int n, ...)
+{}
