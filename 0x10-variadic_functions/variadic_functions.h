b8c0e5a282efa2c8bd579b865f07a256a2a93cdb
mwamba <mwambajoakim@gmail.com>
2025-04-12 02:41:59 +0300

    Declare variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+const char *separator = "";
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
