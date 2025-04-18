a6398ba9e2afc78f65d7767aeec6a931bb3b09ae
mwamba <mwambajoakim@gmail.com>
2025-04-18 22:41:29 +0300

    Print first 50 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,4 +6,30 @@
  * Return: 0 is success.
  */
 int main(void)
-{}
+{
+	long int a = 1;
+	long int b = 2;
+	long int c = 0;
+	long int sum = 0;
+
+	while (c < 50)
+	{
+		if (c == 0)
+			printf("%ld, ", a);
+		else if (c == 1)
+			printf("%ld, ", b);
+		else
+		{
+			sum = a + b;
+			if (c == 50 - 1)
+				printf("%ld", sum);
+			else
+				printf("%ld, ", sum);
+			a = b;
+			b = sum;
+		}
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
