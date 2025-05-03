5403e9b34e9ffb55b2191faf292dbbf360a0933d
mwamba <mwambajoakim@gmail.com>
2025-05-03 21:20:14 +0300

    Count number of nodes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,4 +7,14 @@
  * Return: Number of nodes.
  */
 size_t list_len(const list_t *h);
-{}
+{
+	/*const list_t temp  = h;*/
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
