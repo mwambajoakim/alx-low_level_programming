b3b67d7f79840167beb71eb4cd3dada4842aa848
mwamba <mwambajoakim@gmail.com>
2025-05-03 21:14:35 +0300

    Describe function working

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * list_len - Return number of nodes in a list.
+ * @h: Pointer to a struct (node).
+ *
+ * Return: Number of nodes.
+ */
+size_t list_len(const list_t *h);
+{}
