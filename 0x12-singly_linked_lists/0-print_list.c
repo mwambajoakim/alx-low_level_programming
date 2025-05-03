a46d15e916cbc067789b2f737b36caf7e0cd70df
mwamba <mwambajoakim@gmail.com>
2025-05-03 20:51:47 +0300

    Print nodes of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,4 +7,22 @@
  * Return: Elements of list.
  */
 size_t print_list(const list_t *h)
-{}
+{
+	const list_t *temp = h;
+	unsigned int i, count = 0;
+
+	while (temp != NULL)
+	{
+		if (str == NULL)
+			printf("[0](nil)\n");
+		else
+		{
+			for (i = 0; temp->str[i] != '\0'; i++)
+			;
+			printf("[%d] %s", temp->len, temp->str);
+		}
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
