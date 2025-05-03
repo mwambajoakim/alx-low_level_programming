af1dc5066faa1ae55e4ac22ae37c0fc7f0e8892b
mwamba <mwambajoakim@gmail.com>
2025-05-03 21:31:03 +0300

    Describe function working

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node - Creates node at the beggining of list.
+ * @head: Point to head node.
+ * @str: Pointer to string data in node.
+ *
+ * Return: Pointer to a node.
+ */
+list_t *add_node(const list_t ** head, const char *str)
+{
+
+}
