856c47435ddc41db73f454f63d6f92d2d9480a56
mwamba <mwambajoakim@gmail.com>
2025-05-15 10:29:18 +0300

    Correct typo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -38,7 +38,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		return (NULl);
+		return (NULL);
 	}
 	ptr = strdup(str);
 	if (ptr == NULL)
