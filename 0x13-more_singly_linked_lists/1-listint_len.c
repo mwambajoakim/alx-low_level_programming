15b4cc6fbd38d79de0ea78a0fdd05494f138ecf9
mwamba <mwambajoakim@gmail.com>
2025-05-22 21:07:17 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns number of nodes
+ * @h: Pointer to head node
+ *
+ * Return: Number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+
+}
