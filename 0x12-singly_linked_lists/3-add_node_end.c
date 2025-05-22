04c88b39bd15062167779a599bffccafe2a84081
mwamba <mwambajoakim@gmail.com>
2025-05-22 17:32:57 +0300

    Return new node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -54,7 +54,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
 	temp = *head;
 	while (temp->next != NULL)
