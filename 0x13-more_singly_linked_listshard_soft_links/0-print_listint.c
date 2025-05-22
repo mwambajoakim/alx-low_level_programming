27cef2f7082017ab55c2d7822a526983668ffb79
mwamba <mwambajoakim@gmail.com>
2025-05-22 20:33:22 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_listshard_soft_links/0-print_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints the elements of a list
+ * @h: Pointer to head node
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+
+}
