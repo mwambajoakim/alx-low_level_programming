91497e3623effc197f5bab43b4186af039d0b355
mwamba <mwambajoakim@gmail.com>
2025-05-22 17:37:14 +0300

    Free a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,4 +5,16 @@
  * @head: Pointer to head of list
  */
 void free_list(list_t *head)
-{}
+{
+
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next; /* Save next pointer before freeing */
+		free(head->str);   /* Free the string if it exists */
+		free(head);        /* Free the current node */
+		heada = tmp;       /* Move to next node */
+	}
+
+}
