3538cf7b3ae366baa60c0dfa408596a0a8855a22
mwamba <mwambajoakim@gmail.com>
2025-05-22 20:56:59 +0300

    Add semicolon; resolve bug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,7 +14,7 @@ size_t print_listint(const listint_t *h)
 	if (h == NULL)
 		return (count);
 
-	tmp = h
+	tmp = h;
 	for (; tmp != NULL; count++, tmp = tmp->next)
 		printf("%d\n", tmp->n);
 	return (count);
