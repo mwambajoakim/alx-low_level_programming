f01f1af42173450d42f9511cafb9f7e93ef180e4
mwamba <mwambajoakim@gmail.com>
2025-05-23 17:04:19 +0300

    Sum data in a list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,5 +8,19 @@
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *tmp;
+	int sum = 0;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
