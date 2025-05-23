6a6ecede2fac809359f1585766c8ed84bdc74440
mwamba <mwambajoakim@gmail.com>
2025-05-23 19:05:02 +0300

    Add node if list is empty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	tmp = *head;
 	while (tmp != NULL && i < idx)
 	{
