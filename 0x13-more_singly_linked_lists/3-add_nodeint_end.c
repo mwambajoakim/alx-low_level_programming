b63187ff053035f920fd7959a5d2fcc3571c1a3b
mwamba <mwambajoakim@gmail.com>
2025-05-23 12:52:49 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node to list end
+ * @head: Pointer to head node
+ * @n: Element of a node
+ *
+ * Return: Pointer to node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+}
