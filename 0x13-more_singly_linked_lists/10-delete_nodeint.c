a82654006ff6a010f87b5768c9b96e043abd10a0
mwamba <mwambajoakim@gmail.com>
2025-05-23 19:14:36 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Delete node at given index
+ * @head: Pointer to the address of head node
+ * @index: Index of node
+ *
+ * Return: 1 if successful, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}
