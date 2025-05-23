3f1740beb9d58aa4d5371d9b54163f66074647ff
mwamba <mwambajoakim@gmail.com>
2025-05-23 16:59:56 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sums the data in a list
+ * @head: Pointer to head node
+ *
+ * Return: Sum of the data in the nodes
+ */
+int sum_listint(listint_t *head)
+{
+
+}
