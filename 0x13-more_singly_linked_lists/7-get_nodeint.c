a20792c00da070b064827d2426da8ac0c23a33a7
mwamba <mwambajoakim@gmail.com>
2025-05-23 16:30:00 +0300

    Describe function working

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Get a node at index index
+ * @head: Pointer to head node
+ * @index: Pointer to node index
+ *
+ * Return: The node searched for
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+}
