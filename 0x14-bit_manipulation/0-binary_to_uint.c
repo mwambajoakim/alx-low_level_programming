f7d4251b7c0cbb4916a5b90a3bb17dbebf074cfa
mwamba <mwambajoakim@gmail.com>
2025-05-27 15:14:34 +0300

    Describe function working

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary number to unsigned int
+ * @b: Pointer string of binary numbers
+ *
+ * Return: An unsigned integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+}
