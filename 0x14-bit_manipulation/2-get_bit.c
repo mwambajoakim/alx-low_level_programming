8e1b9ea50eb191ba2257bbbbde5ce75ea25338c0
mwamba <mwambajoakim@gmail.com>
2025-05-27 18:45:46 +0300

    Describe function working

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns value of bit at given index
+ * @n: Integer
+ * @index: Index of bit
+ *
+ * Return: Bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+}
