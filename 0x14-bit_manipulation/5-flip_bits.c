4380ea24174221a9eb4335005d64cb6c8648bafe
mwamba <mwambajoakim@gmail.com>
2025-05-28 17:01:19 +0300

    Describe function working

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * flip_bits - Checks number of bits to flip to get another number
+ * @n: Integer one
+ * @m: Integer two
+ *
+ * Return: Number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{}
