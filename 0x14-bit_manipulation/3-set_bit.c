c36144fa361be69a559003d3b1cb9c1ca7a8b2ff
mwamba <mwambajoakim@gmail.com>
2025-05-28 15:25:24 +0300

    Describe function working

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets value of bit to 1
+ * @n: Integer
+ * @index: Index of bit
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+}
