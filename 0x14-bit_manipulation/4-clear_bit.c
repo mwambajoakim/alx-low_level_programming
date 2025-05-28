5657dab1aa3f49103987abf0673bdbd182d8e448
mwamba <mwambajoakim@gmail.com>
2025-05-28 16:09:39 +0300

    Describe function working

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit to 0
+ * @n: Integer
+ * @index: Index of bit
+ *
+ * Return: 1 if successful, -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{}
