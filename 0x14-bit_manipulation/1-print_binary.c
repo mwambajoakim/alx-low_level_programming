d1b072b3d6e1d32abb2999553770e41ad918153c
mwamba <mwambajoakim@gmail.com>
2025-05-28 14:56:17 +0300

    Print binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,5 +6,22 @@
  */
 void print_binary(unsigned long int n)
 {
+	int i, start = 0;
 
+	for (i = sizeof(n) * CHAR_BIT - 1; i >= 0; --i)
+	{
+		if (n & (1UL << i))
+		{
+			_putchar("1");
+			start = 1;
+		}
+		else if (start)
+		{
+			_putchar("0");
+		}
+		if(!start)
+		{
+			_putchar("0");
+		}
+	}
 }
