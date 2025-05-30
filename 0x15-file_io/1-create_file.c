e3c3a9a5df4e4f27d03953e2e78dc5eaf2f07cbe
mwamba <mwambajoakim@gmail.com>
2025-05-30 18:45:51 +0300

    Use open syscall to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,22 +18,17 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		opfile = creat(filename, 0600);
+		opfile = open(filename, O_CREAT, 0600);
 		return (1);
 	}
 	opfile = open(filename, O_TRUNC);
 	if (opfile < 0)
 	{
-		opfile = creat(filename, 0600);
-		if (opfile < 0)
-		{
-			return (-1);
-		}
+		opfile = open(filename, O_CREAT, 0600);
 		write(opfile, text_content, length);
 		close(opfile);
 		return (1);
 	}
-	write(opfile, text_content, length);
 	close(opfile);
 	return (1);
 }
