8eb3432ad611c84ff1b2198d48d81150ec11460d
mwamba <mwambajoakim@gmail.com>
2025-05-30 20:21:18 +0300

    Keep the permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		opfile = open(filename, O_CREAT, 0600);
 		return (1);
 	}
-	opfile = open(filename, O_TRUNC | O_WRONLY);
+	opfile = open(filename, O_TRUNC | O_WRONLY, 0600);
 	if (opfile < 0)
 	{
 		opfile = open(filename, O_CREAT, 0600);
