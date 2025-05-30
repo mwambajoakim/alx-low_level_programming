015217266c0bd8aa7eb2fbe065b1a88bbb012b4b
mwamba <mwambajoakim@gmail.com>
2025-05-30 17:00:02 +0300

    Describe function working

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: File name
+ * @text_content: Text content
+ *
+ * Return: 1 on success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{}
