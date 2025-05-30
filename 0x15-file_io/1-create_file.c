24952be3ba854460179b862a11882f5e6d21432a
mwamba <mwambajoakim@gmail.com>
2025-05-30 18:21:02 +0300

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,8 +30,10 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		write(opfile, text_content, length);
+		close(opfile);
 		return (1);
 	}
 	write(opfile, text_content, length);
+	close(opfile);
 	return (1);
 }
