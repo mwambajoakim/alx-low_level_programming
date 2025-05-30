4cac31acaa0502ba5247fc38e2f5e43e966b05ec
mwamba <mwambajoakim@gmail.com>
2025-05-30 18:19:28 +0300

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,4 +8,30 @@
  * Return: 1 on success, -1 otherwise
  */
 int create_file(const char *filename, char *text_content)
-{}
+{
+	int opfile;
+	size_t length = strlen(text_content);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		opfile = creat(filename, 0600);
+		return (1);
+	}
+	opfile = open(filename, O_TRUNC);
+	if (opfile < 0)
+	{
+		opfile = creat(filename, 0600);
+		if (opfile < 0)
+		{
+			return (-1);
+		}
+		write(opfile, text_content, length);
+		return (1);
+	}
+	write(opfile, text_content, length);
+	return (1);
+}
