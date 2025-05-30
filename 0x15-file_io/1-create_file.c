dcc864b4350ec6e4c3ad8c6afedfbc9efe63e311
mwamba <mwambajoakim@gmail.com>
2025-05-30 18:53:02 +0300

    Open file to write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		opfile = open(filename, O_CREAT, 0600);
 		return (1);
 	}
-	opfile = open(filename, O_TRUNC);
+	opfile = open(filename, O_WRONLY);
 	if (opfile < 0)
 	{
 		opfile = open(filename, O_CREAT, 0600);
