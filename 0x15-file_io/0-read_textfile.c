366c0ff43aac13ca93cb1bd3cabff2b7b46d1b52
mwamba <mwambajoakim@gmail.com>
2025-05-30 14:34:33 +0300

    Describe function working

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * read_textfile - Writes text to stdout
+ * @filename: Pointer to the file
+ * @letters: Number of letters to write
+ *
+ * Return the number of letters written
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+{}
