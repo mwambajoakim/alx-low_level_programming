4804414791b32d0b1685a2ce74f8adfb780eaab5
mwamba <mwambajoakim@gmail.com>
2025-06-01 18:03:13 +0300

    Append content to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,4 +8,29 @@
  * Return: 1 if successful, -1 otherwise
  */
 int append_text_to_file(const char *filename, char *text_content)
-{}
+{
+	int opfile;
+	ssize_t length, written_b;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	opfile = open(filename, O_WRONLY | O_APPEND, 0222);
+	if (opfile < 0)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+		written_b = write(opfile, text_content, length);
+		if (written_b != length)
+		{
+			close(opfile);
+			return (-1);
+		}
+	}
+	close(opfile);
+	return (1);
+}
