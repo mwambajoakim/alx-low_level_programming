387134e5f2fb176a99440382b943335a83eb81d4
mwamba <mwambajoakim@gmail.com>
2025-06-01 18:15:24 +0300

    Describe program working

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Copies contents of a file to another
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char **argv[])
+{}
