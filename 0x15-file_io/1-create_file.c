39b2a301692da6760a7673876186b73c51d72793
mwamba <mwambajoakim@gmail.com>
2025-06-01 17:44:30 +0300

    Remove repetition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,29 +10,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int opfile;
-	size_t length = strlen(text_content);
+	size_t length;
+	ssize_t written_b;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		opfile = open(filename, O_CREAT, 0600);
-		close(opfile);
-		return (1);
-	}
-	opfile = open(filename, O_TRUNC | O_WRONLY);
+	
+	opfile = open(filename, O_CREAT, O_TRUNC | O_WRONLY, 0600);
 	if (opfile < 0)
 	{
-		opfile = open(filename, O_CREAT | O_WRONLY, 0600);
-		if (!opfile)
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+		written_b = write(opfile, text_content, length);
+		if (written_b != length)
 			return (-1);
-		write(opfile, text_content, length);
-		close(opfile);
-		return (1);
 	}
 	write(opfile, text_content, length);
 	close(opfile);
-	return (-1);
+	return (1);
 }
