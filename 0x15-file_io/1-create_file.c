c37d5e02e5b8ceb028d2fb806eeb25e4a5681a7a
mwamba <mwambajoakim@gmail.com>
2025-06-01 17:44:45 +0300

    Remove repetition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	
-	opfile = open(filename, O_CREAT, O_TRUNC | O_WRONLY, 0600);
+	opfile = open(filename, O_CREAT| O_TRUNC | O_WRONLY, 0600);
 	if (opfile < 0)
 	{
 		return (-1);
