49d98f5a439d460ef3f1bddca37a3d9a841e167d
mwamba <mwambajoakim@gmail.com>
2025-06-01 15:31:27 +0300

    Create filename if it does not exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,11 +27,9 @@ int create_file(const char *filename, char *text_content)
 		opfile = open(filename, O_TRUNC | O_WRONLY, 0600);
 		if (opfile < 0)
 		{
-			opfile = open(filename, O_CREAT, 0600);
-			write(opfile, text_content, length);
-			close(opfile);
-			return (1);
+			return (-1);
 		}
+		opfile = open(filename, O_CREAT, 0600);
 		write(opfile, text_content, length);
 		close(opfile);
 		return (1);
