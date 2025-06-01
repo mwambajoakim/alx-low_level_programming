29ad978e2bdb554408c3da809d475b73a6916bbd
mwamba <mwambajoakim@gmail.com>
2025-06-01 17:53:39 +0300

    Describe function working

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to a file
+ * @filename: File name
+ * @text_content: Text content to append
+ *
+ * Return: 1 if successful, -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{}
