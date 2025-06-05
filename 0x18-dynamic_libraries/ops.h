2dc39d86cf74affcdb9b5fd7811150b86a25b5c5
mwamba <mwambajoakim@gmail.com>
2025-06-05 10:42:37 +0300

    Define header function

---
+++ 0x18-dynamic_libraries/ops.h
@@ -0,0 +1,13 @@
+#ifndef OPS_H
+#define OPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* OPS_H */
