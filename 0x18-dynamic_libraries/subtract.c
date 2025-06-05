36888742622f9a603c9e983412b000c662a30d61
mwamba <mwambajoakim@gmail.com>
2025-06-05 10:48:24 +0300

    Subtract integers

---
+++ 0x18-dynamic_libraries/subtract.c
@@ -0,0 +1,13 @@
+#include "ops.h"
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a minus b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
