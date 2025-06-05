ea94af7740477454383820a36d414b5ee9bf6511
mwamba <mwambajoakim@gmail.com>
2025-06-05 10:47:55 +0300

    Divide integers

---
+++ 0x18-dynamic_libraries/divide.c
@@ -0,0 +1,18 @@
+#include "ops.h"
+
+/**
+ * div - Divides two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The quotient of a and b.
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		fprintf(stderr, "Error: Division by zero\n");
+		exit(EXIT_FAILURE);
+	}
+	return (a / b);
+}
