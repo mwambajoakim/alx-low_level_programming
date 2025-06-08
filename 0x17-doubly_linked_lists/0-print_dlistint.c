cab527955ffdcbdf606ad955fa80152228f785a8
mwamba <mwambajoakim@gmail.com>
2025-06-08 19:12:09 +0300

    Describe function working

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_d_listint - Prints the elements in a list
+ * @h: Pointer to the heade node
+ *
+ * Return: Number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+}
