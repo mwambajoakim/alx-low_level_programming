3bf8052bad22c37698456bd0f0e6353de761021c
mwamba <mwambajoakim@gmail.com>
2025-06-08 19:42:59 +0300

    Describe function working

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node to beginning of list
+ * @head: Pointer to head node
+ * @n: Integer to add to the new node
+ *
+ * Return: Pointer to the new node or NULL if it fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{}
