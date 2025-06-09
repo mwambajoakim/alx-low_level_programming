61dc68b49985977a26f2a34e7d1d446e1ad5ddcd
mwamba <mwambajoakim@gmail.com>
2025-06-09 13:24:42 +0300

    Return node in list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,30 @@
 #include "lists.h"
 
 /**
- * get_dnode_at_index - Returns node at index
+ * get_dnodeint_at_index - Returns node at index
  * @head: Pointer to head node
  * @index: Index of node
  *
- * Returns the node
+ * Return: Returns the node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{}
+{
+	unsigned int i = 0;
+	dlistint_t *temp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	temp = head;
+	while (temp != NULL && i < index)
+	{
+		++i;
+		temp = temp->next;
+	}
+	if (i == index)
+		return (temp);
+
+	return (temp);
+}
