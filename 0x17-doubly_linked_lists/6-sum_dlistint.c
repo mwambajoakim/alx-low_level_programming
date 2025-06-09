88fe43fadc5158ff33d721270a0f0bab1445a823
mwamba <mwambajoakim@gmail.com>
2025-06-09 13:29:37 +0300

    Describe function working

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sums the elements of a node
+ * @head: Pointer to head node
+ *
+ * Return: Sum of elements
+ */
+int sum_dlistint(dlistint_t *head)
+{}
