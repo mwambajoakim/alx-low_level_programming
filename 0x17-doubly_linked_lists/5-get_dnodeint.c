5c523d4068ad586d17cd0f7183a6ac1728369f2b
mwamba <mwambajoakim@gmail.com>
2025-06-09 12:03:08 +0300

    Describe function working

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * get_dnode_at_index - Returns node at index
+ * @head: Pointer to head node
+ * @index: Index of node
+ *
+ * Returns the node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{}
