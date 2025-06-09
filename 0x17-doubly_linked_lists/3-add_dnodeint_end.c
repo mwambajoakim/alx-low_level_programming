d67525de4e2e0870fd026781f68cee1d73beb38a
mwamba <mwambajoakim@gmail.com>
2025-06-09 11:23:41 +0300

    Remove bug

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	end_node = malloc(sizeof(d_listint_t));
+	end_node = malloc(sizeof(dlistint_t));
 	if (end_node == NULL)
 	{
 		return (NULL);
