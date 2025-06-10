459a6e360f19b1e826b2acc249e10caefcc8f2ff
mwamba <mwambajoakim@gmail.com>
2025-06-10 15:50:31 +0300

    If list empty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	if (*h == NULL)
+	if (*h == NULL && idx != 0)
 	{
 		*h = new_node;
 		return (new_node);
