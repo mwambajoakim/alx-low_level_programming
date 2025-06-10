45949324c6341becf81f2615e0caac4f5bea4aba
mwamba <mwambajoakim@gmail.com>
2025-06-10 15:48:02 +0300

    Add get len function

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Prints the number of wlwmwnts in a list
+ * @h: Pointer to the head node
+ *
+ * Return: Number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts a node at index idx
  * @h: Pointer to head node
  * @idx: Index at which to insert
