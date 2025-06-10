dac6efdc802e22d84564168f75c9c59467db02d0
mwamba <mwambajoakim@gmail.com>
2025-06-10 15:10:48 +0300

    Add node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,4 +9,42 @@
  * Return: Pointer to the node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-()
+{
+	unsigned int i = 0;
+	dlistint_t *new_node, *temp, *prev_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
+	temp = *h;
+	while (temp != NULL && i <= idx)
+	{
+		prev_node = temp;
+		++i;
+		temp = temp->next;
+	}
+	if (i != idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	prev_node->next = new_node;
+	new_node->prev = prev_node;
+	new_node->next = temp;
+	return (new_node);
+}
