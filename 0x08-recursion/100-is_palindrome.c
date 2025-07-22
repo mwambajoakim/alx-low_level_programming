f77a195e7c22f9baf085affecbd679680c1be386
mwamba <mwambajoakim@gmail.com>
2025-07-22 15:06:50 +0300

    Define function working

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ *
+ * @s: Pointer to a string.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+}
