081c141a3ee0241bc66dfdc549f525511effe373
mwamba <mwambajoakim@gmail.com>
2025-07-26 18:15:15 +0300

    Describe function working

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sum of diagonals of a matrix
+ * @a: Pointer to the matrix.
+ * @size: Size of the matrix.
+ */
+void print_diagsums(int *a, int size)
+{
+}
